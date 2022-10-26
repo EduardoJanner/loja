82b08ac1719e9b95ad20e36895be87fb17e1249c
EduardoJanner <eduardojanner2014@gmail.com>
2022-10-26 12:38:11 -0300

    criando o widget

--- lib/src/auth/sing_in_screen.dart
+++ lib/src/auth/sing_in_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loja/src/auth/components/custom_text_field.dart';
 class SignInScreen extends StatelessWidget {
   const SignInScreen({super.key});
 
@@ -28,22 +29,15 @@ class SignInScreen extends StatelessWidget {
               child: Column(
                 children:[
                   // Email
-                    TextFormField(
-                      decoration: InputDecoration(
-                        isDense: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(18),
-                        ),
-                      ),
-                    ),  
+                    CustomTextField(
+                      icon: Icons.email,
+                      label: 'Email',
+                    ),
                   // Senha
-                    TextFormField(
-                      decoration: InputDecoration(
-                        isDense: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(18),
-                        ),
-                      ),
+                    CustomTextField(
+                      icon: Icons.lock,
+                      label: 'Senha',
+                      isObscure: true
                     ),
                 ],
               ),  
