e669938d1ad90d606fcd2c27eecc259d131bf70b
EduardoJanner <eduardojanner2014@gmail.com>
2022-10-26 21:33:32 -0300

    botão esqueci a senha

--- lib/src/auth/sing_in_screen.dart
+++ lib/src/auth/sing_in_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:loja/src/auth/components/custom_text_field.dart';
+
 class SignInScreen extends StatelessWidget {
   const SignInScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    backgroundColor: Colors.green,
+      backgroundColor: Colors.green,
       body: Column(
         children: [
           Expanded(
@@ -16,32 +17,59 @@ class SignInScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 32,
-              vertical: 40,
-            ),
-              decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(45),
-              )
+              padding: const EdgeInsets.symmetric(
+                horizontal: 32,
+                vertical: 40,
               ),
-
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(45),
+                  )),
               child: Column(
-                children:[
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
                   // Email
-                    CustomTextField(
-                      icon: Icons.email,
-                      label: 'Email',
-                    ),
+                  const CustomTextField(
+                    icon: Icons.email,
+                    label: 'Email',
+                  ),
                   // Senha
-                    CustomTextField(
-                      icon: Icons.lock,
-                      label: 'Senha',
-                      isSecret: true
+                  const CustomTextField(
+                      icon: Icons.lock, label: 'Senha', isSecret: true),
+
+                  SizedBox(
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(17),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Entrar',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
                     ),
+                  ),
+
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Esqueceu a Senha?',
+                        style: TextStyle(
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
-              ),  
+              ),
             ),
           ),
         ],
